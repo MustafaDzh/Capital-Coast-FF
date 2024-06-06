6e194f2aa9267563b02ad420cf9cc032fce8082d
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-06-06 15:03:38 +0300

    header-responsive

--- js/index.js
+++ js/index.js
@@ -5,3 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
         phase.style.backgroundColor = '#68C5F9';
     });
 });
+
+//nav-mobile
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const mobileNav = document.querySelector('.mobile-nav');
+
+    console.log('DOM fully loaded and parsed');
+
+    hamburgerBtn.addEventListener('click', function() {
+        console.log('Hamburger button clicked');
+        mobileNav.classList.toggle('open');
+    });
+});
+
+
