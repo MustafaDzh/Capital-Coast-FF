163cad6aaa6a1dc580c9f8102dd2fdbaa1c314c4
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-06-07 12:12:23 +0300

    phases(not full), stages, news

--- js/index.js
+++ js/index.js
@@ -20,24 +20,40 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //swiper for phases
+// document.addEventListener('DOMContentLoaded', function () {
+//     var phasesContainer = document.querySelector('.phases');
+//     var mobileNavArrow = document.querySelector('.mobile-nav-arrow');
+//     var phases = document.querySelectorAll('.phase');
+//     var currentPhaseIndex = 0;
+
+//     if (phasesContainer && mobileNavArrow) {
+//         mobileNavArrow.addEventListener('click', function () {
+//             phases[currentPhaseIndex].style.display = 'none';
+//             currentPhaseIndex = (currentPhaseIndex + 1) % phases.length;
+//             phases[currentPhaseIndex].style.display = 'block';
+//         });
+//     } else {
+//         console.log('One or both elements not found.');
+//     }
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
     var swiperContainer = document.querySelector('.phases');
-    var mobileNavArrow = document.querySelector('.mobile-nav-arrow');
+    var mobileNavArrow = document.querySelector('#mobile-nav-arrow');
 
     if (swiperContainer && mobileNavArrow) {
         var swiper = new Swiper(swiperContainer, {
             direction: 'horizontal',
-            loop: false,
-            pagination: {
-                el: '.swiper-pagination',
-            },
+            loop: true,
         });
 
         mobileNavArrow.addEventListener('click', function () {
             console.log('Clicked on mobile navigation arrow');
-            swiper.slideNext(); 
+            swiper.slideNext();
         });
-    } 
+    }
 });
+
+
 
 
